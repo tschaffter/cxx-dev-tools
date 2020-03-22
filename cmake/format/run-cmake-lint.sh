1079f68b77599d489ee9c665ea7cc70163e66134
tschaffter <thomas.schaffter@gmail.com>
2020-03-22 14:29:52 -0700

    Fix cmake lint

--- cmake/format/run-cmake-lint.sh
+++ cmake/format/run-cmake-lint.sh
@@ -6,5 +6,4 @@ cmake-lint \
     ${CMAKE_FILES} \
     --config-files \
         ${CXX_DEV_TOOLS_PATH}/cmake/format/.cmake-format.json \
-        ${CXX_DEV_TOOLS_PATH}/cmake/format/.esp-idf-cmake-format.json \
-    --max-statement-spacing 2
+        ${CXX_DEV_TOOLS_PATH}/cmake/format/.esp-idf-cmake-format.json
