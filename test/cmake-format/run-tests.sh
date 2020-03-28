01a33296e9c83253f40c983ad427dd448227eda7
tschaffter <thomas.schaffter@gmail.com>
2020-03-28 11:32:48 -0700

    Add cmake/bin/cxxdt-cmake-format

--- test/cmake-format/run-tests.sh
+++ test/cmake-format/run-tests.sh
@@ -3,15 +3,15 @@
 cd ${CXX_DEV_TOOLS_PATH}/test/cmake-format
 
 # Test 1
-run-cmake-format-check.sh samples/valid-format.cmake
+cxxdt-cmake-lint samples/valid-format.cmake
 if [ $? -ne 0 ]; then
-    echo "cmake-format-check.sh failed to return 0 on valid cmake sample"
+    echo "cxxdt-cmake-lint failed to return 0 on valid cmake sample"
     exit 1
 fi
 
 # Test 2
-run-cmake-format-check.sh samples/invalid-format.cmake
+cxxdt-cmake-lint samples/invalid-format.cmake
 if [ $? -eq 0 ]; then
-    echo "cmake-format-check.sh failed to return not 0 on invalid cmake sample"
+    echo "cxxdt-cmake-lint failed to return not 0 on invalid cmake sample"
     exit 1
 fi
